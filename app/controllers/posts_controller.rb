c3941e227f4f46975324e3d37f97d31716e63cc7
meganthadev <meganthadev@gmail.com>
2020-06-29 14:09:47 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,5 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
-  
-  def create
-    @post = Post.new(post_params)
-      if @post.valid?
-       @post.save
-       redirect_to post_path(@post)
-      else 
-        render :new
-    end
-  end 
-  
-  def index 
-    @authors = Author.all 
-  end 
-  
   def show
   end
 
@@ -22,24 +7,20 @@ class PostsController < ApplicationController
   end
 
   def update
-    @article = Post.find(params[:id])
-      if @article.valid?
-       @article.save
-       redirect_to post_path(@article)
-      else 
-        render :edit
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
     end
-  end 
-
+  end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
   end
-  
+
   def set_post!
     @post = Post.find(params[:id])
   end
-  
 end
